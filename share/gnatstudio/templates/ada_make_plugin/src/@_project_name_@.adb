400e6fb1aafd07a64dc2a37f7e2cb9fb383d3bfb
persan <per.s.sandberg@bahnhof.se>
2020-03-31 20:07:09 +0200

    Clean up template

--- share/gnatstudio/templates/ada_make_plugin/src/@_project_name_@.adb
+++ share/gnatstudio/templates/ada_make_plugin/src/@_project_name_@.adb
@@ -1,8 +1,9 @@
 with Gnumake_Plugin.Generic_Plugin;
+with Gnumake_Plugin.Image;
 package body @_Project_Name_@ is
-
+   use Gnumake_Plugin;
    -- Instaniate to register the methods.
-   package Plugin is new Gnumake_Plugin.Generic_Plugin
+   package Plugin is new Generic_Plugin
      (Name     => "sample",
       Min_Args => 1,
       Max_Args => 0,
@@ -15,16 +16,16 @@ package body @_Project_Name_@ is
    --------------
 
    function Sample
-     (Name : String; Argv : Gnumake_Plugin.String_Vectors.Vector) return Gnumake_Plugin.String_Vectors.Vector
+     (Name : String; Argv : Gnumake_Plugin.String_Vectors.Vector) return String
    is
+      Ret : Gnumake_Plugin.String_Vectors.Vector;
    begin
-      return Ret : Gnumake_Plugin.String_Vectors.Vector do
-         --  Do something usefull
-         --  
-         for I of Argv loop
-            Ret.Append ("___" & I & "___");
-         end loop;
-      end return;
+      --  Do something usefull
+      --  
+      for I of Argv loop
+         Ret.Append ("___" & I & "___");
+      end loop;
+      return Image(ret);
    end Sample;
 
 end @_Project_Name_@;
--- share/gnatstudio/templates/ada_make_plugin/src/@_project_name_@.ads
+++ share/gnatstudio/templates/ada_make_plugin/src/@_project_name_@.ads
@@ -1,4 +1,4 @@
 with Gnumake_Plugin.String_Vectors;
 package @_Project_Name_@ is
-   function Sample (Name : String; Argv : Gnumake_Plugin.String_Vectors.Vector) return  Gnumake_Plugin.String_Vectors.Vector;
+   function Sample (Name : String; Argv : Gnumake_Plugin.String_Vectors.Vector) return  String;
 end @_Project_Name_@;
